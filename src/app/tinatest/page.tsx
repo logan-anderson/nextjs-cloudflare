50f8e87230bb261947d2afd0797f4e77b6491ed8
Logan Anderson <landerson2@upei.ca>
2023-11-20 09:49:21 -0500

    add tina test page

---
+++ src/app/tinatest/page.tsx
@@ -0,0 +1,14 @@
+import client from "../../../tina/__generated__/client";
+
+export default async function TinaTestPage() {
+  const data = await client.queries.postConnection();
+  const str = JSON.stringify(data?.data, null, 2);
+
+  return (
+    <div>
+      <pre>{str}</pre>
+    </div>
+  );
+}
+
+export const runtime = "edge";
