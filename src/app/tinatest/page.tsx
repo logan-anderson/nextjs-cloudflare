e3a3324e5d4103551970f441479c43bbe0fc25b4
Logan Anderson <landerson2@upei.ca>
2023-11-21 14:55:39 -0500

    add self hosted files

--- src/app/tinatest/page.tsx
+++ src/app/tinatest/page.tsx
@@ -1,4 +1,4 @@
-import client from "../../../tina/__generated__/client";
+import client from "../../../tina/__generated__/databaseClient";
 
 export default async function TinaTestPage() {
   const data = await client.queries.postConnection();
